0fdf29ea0676443bb31cd8fe8c5252d1e6877f71
gyoza <gyoza@gyoza.gyoza>
2017-12-29 13:30:38 -0800

    updated some shitttt

--- get_coinz.py
+++ get_coinz.py
@@ -8,12 +8,12 @@ poop = defaultdict(dict)
 poop["turds"] = json.loads(urllib2.urlopen('https://api.coinmarketcap.com/v1/ticker/').read())
 
 wanted = [ "siacoin", "litecoin", "bitcoin", "vertcoin", "dogecoin", "antshares", "ripple", "neo", "iota", "stellar", 'raiblocks']
-print "   Items represented in USD.\n"
 table_data = []
 for each in poop["turds"]:
   for wants in wanted:
       if each["id"] == wants:
-            coin = each["id"] + "  ({})".format(each["symbol"])
+            coin = each["id"]
+            symbol = "({})".format(each["symbol"])
             price = each["price_usd"]
             if "-" in each["percent_change_1h"]:
               onehour = "↓ {}%".format(each["percent_change_1h"].replace('-', ''))
@@ -27,12 +27,19 @@ for each in poop["turds"]:
               sevendays = "↓ {}%".format(each["percent_change_7d"].replace('-', ''))
             else:
               sevendays = "↑ {}%".format(each["percent_change_7d"])
-            table_data.append([coin, price, onehour, twentyfour, sevendays])
-            col_width = max(len(word) for row in table_data for word in row) + 5  # padding
-table_data.sort(key=lambda x: float(x[1]), reverse=True)
-table_data.insert(0, ["coin", "price", "1hour", "24hour", "7days"])
-col_width = max(len(word) for row in table_data for word in row) + 5  # padding
+            table_data.append([coin, symbol, price, onehour, twentyfour, sevendays])
+table_data.sort(key=lambda x: float(x[2]), reverse=True)
+table_data.insert(0, ["coin", "symbol",  "price", "1hour", "24hour", "7days"])
+col_width = max(len(word) for row in table_data for word in row) + 2
+print "--------------------- ---- ----- -- ---- - -- -   -\n"
+first = True
 for row in table_data:
-  final = "".join(word.decode('utf-8').ljust(col_width) for word in row)
-  print final.encode('utf-8')
-
+  if first:
+    final = "".join(word.decode('utf-8').ljust(col_width) for word in row)
+    print final.encode('utf-8')
+    print "--------------------- ---- ----- -- ---- - -- -   -"
+    first = False
+  else:
+    final = "".join(word.decode('utf-8').ljust(col_width) for word in row)
+    print final.encode('utf-8')
+print "\n[  Item value represented in USD.  ]\n"
