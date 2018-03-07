34fb83a7dd548643eb782d49ce3cfcf93650ebdb
gyoza <gyoza@users.noreply.github.com>
2018-03-06 22:29:51 -0800

    fixed up some shit!
    Allowed for multiple highlights per msg now, made parsing easier for consuming yay

--- pretty_logger.py
+++ pretty_logger.py
@@ -12,23 +12,26 @@ class PrettyLogger(object):
     header = 'your header'
 
     _log_level_map = {
-        "error": ["red", "\x1b[31m"],
-        "warn": ["yellow", "\x1b[33m"],
-        "info": ["green", "\x1b[32m"],
-        "other": ["cyan", "\x1b[36m"],
-        "invalidloglevel": ["red", "\x1b[36m"],
-        "creset": ["creset", "\x1b[0m"],
-        "debug": ["magenta", "\x1b[35m"],
+        "error"   : ["red", "\x1b[31m"],
+        "warn"    : ["yellow", "\x1b[33m"],
+        "info"    : ["green", "\x1b[32m"],
+        "verbose" : ["blue", "\x1b[34;1m"],
+        "other"   : ["cyan", "\x1b[36m"],
+        "invalid" : ["red", "\x1b[36m"],
+        "creset"  : ["creset", "\x1b[0m"],
+        "debug"   : ["magenta", "\x1b[35m"],
     }
 
     _color_map = {
         "red": "\x1b[31m",
         "yellow": "\x1b[33m",
         "green": "\x1b[32m",
+        "blue": "\x1b[34m",
         "cyan": "\x1b[36m",
         "magenta": "\x1b[35m",
         "hred": "\x1b[31;1m",
         "hyellow": "\x1b[33;1m",
+        "hblue": "\x1b[34;1m",
         "hgreen": "\x1b[32;1m",
         "hcyan": "\x1b[36;1m",
         "hmagenta": "\x1b[35;1m",
@@ -36,23 +39,23 @@ class PrettyLogger(object):
     }
 
     def highlight(self, msg):
-        if 'H|' in msg:
-            color_code_split = msg.split("|")
-            if len(color_code_split) == 2:
-                color_code = "magenta"
-            else:
-                color_code = msg.split("|")[1]
-            if color_code not in self._color_map:
-                highlight_color = self._color_map["hmagenta"]
-            else:
-                highlight_color = self._color_map[color_code]
+        if msg.startswith("H|"):
             reset_color = self._color_map["creset"]
-            msg = msg.split(' ')
-            for index, items in enumerate(msg):
-                if "H|" in items:
-                    word = '{}{}{}'.format(highlight_color, items.split("|")[-1:][0], reset_color)
+            msg = msg.split("H|")[1]
+            msg = msg.split(" ")
+            msgcopy = msg
+            for index, item in enumerate(msgcopy):
+                print index, item
+                if "|" in item:
+                    color_code = item.split("|")[0].lower()
+                    if color_code not in self._color_map:
+                        highlight_color = self._color_map["hmagenta"]
+                    else:
+                        highlight_color = self._color_map[color_code]
+                    word = item.split("|")[-1:][0]
+                    word = '{}{}{}'.format(highlight_color, word, reset_color)
                     msg[index] = "{}".format(word)
-                    msg = " ".join(msg)
+            msg = " ".join(msg)
         return msg
 
     def fix_header(self):
@@ -61,11 +64,11 @@ class PrettyLogger(object):
     def __init__(self, **kwargs):
         log_level = kwargs["log_level"]
         if log_level not in self._log_level_map:
-            log_level = "invalidloglevel"
+            log_level = "invalid"
         msg = kwargs["msg"]
         self.log_level_color = self._log_level_map[log_level][1]
         self.log_level_color_reset = self._log_level_map["creset"][1]
-        self.log_level = ":{}{:>5s}{}:".format(self.log_level_color, log_level, self.log_level_color_reset)
+        self.log_level = ":{}{:>7s}{}:".format(self.log_level_color, log_level, self.log_level_color_reset)
         header = self.fix_header()
         msg = self.highlight(msg)
         print("{} {} {}").format(header, self.log_level, msg)
